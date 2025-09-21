63ac9857408ef9a8e49bdb05a42b1158c640fbb4
Vadim Sergeev <doorstranger@gmail.com>
2025-09-21 20:35:30 +0300

    POJOs for Courier and Order, URLs in ConfigConstants

---
+++ src/main/java/ru/praktikum/configconstants/ConfigConstants.java
@@ -0,0 +1,15 @@
+package ru.praktikum.configconstants;
+
+public class ConfigConstants {
+
+    public static final String HOST = "https://qa-scooter.praktikum-services.ru/";
+
+    public static final String CREATE_COURIER_ENDPOINT = "/api/v1/courier";
+    public static final String LOGIN_COURIER_ENDPOINT = "/api/v1/courier/login";
+    public static final String DELETE_COURIER_ENDPOINT = "/api/v1/courier/{id}";
+
+    public static final String CREATE_ORDER_ENDPOINT = "/api/v1/orders";
+    public static final String CANCEL_ORDER_ENDPOINT = "/api/v1/orders/cancel";
+    public static final String GET_ORDERS_LIST_ENDPOINT = "/api/v1/orders";
+
+}
